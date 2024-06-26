cc40e28d59f3649bae889b7f58de3c928475bd93
Austin Arrington <austin@teralytic.com>
2024-06-26 17:47:52 -0400

    Update agi-esmc.py

--- agi-esmc.py
+++ agi-esmc.py
@@ -27,17 +27,18 @@ growers = response.json()
 enrollments_by_grower = {}
 
 for grower in growers:
-    partner_grower_id = grower.get('grower_id')
-    grower_name = grower.get('name')
+    partner_grower_id = grower.get('partner_grower_id')
+    grower_name = grower.get('partner_grower_name')
+    grower_id = grower.get('grower_id')
     if not partner_grower_id:
         print("Skipping grower with missing ID")
         continue
 
-    enrollments_url = f'https://sustainability-api.farmobile.com/api/v1/growers/{partner_grower_id}/enrollments'
+    enrollments_url = f'https://sustainability-api.farmobile.com/api/v1/growers/{grower_id}/enrollments'
 
     response = requests.get(enrollments_url, headers=headers)
     if response.status_code == 404:
-        print(f"Enrollments not found for grower ID {partner_grower_id}")
+        print(f"Enrollments not found for partner grower ID {partner_grower_id}")
         continue
 
     response.raise_for_status()
@@ -46,8 +47,8 @@ for grower in growers:
     for enrollment in enrollments:
         enrollment.update({
             "partner_grower_id": partner_grower_id,
-            "grower_id": enrollment.get('grower_id'),
-            "grower_name": grower_name,
+            "partner_grower_name": grower_name,
+            "grower_id": grower_id,
             "partner_field_id": enrollment.get('field_id')
         })
 
@@ -75,7 +76,7 @@ for partner_grower_id, enrollments in enrollments_by_grower.items():
             "enrollment_id": enrollment_id,
             "partner_grower_id": enrollment["partner_grower_id"],
             "grower_id": enrollment["grower_id"],
-            "grower_name": enrollment["grower_name"],
+            "partner_grower_name": enrollment["partner_grower_name"],
             "partner_field_id": enrollment["partner_field_id"],
             "field_events_summary": enrollment.get("field_events_summary", {}),
             "field_events": cpfrs
